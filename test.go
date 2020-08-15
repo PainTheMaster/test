db707ab7b9904b9275250db1bb7d9f6f9557101d
Ken on Latitude <feruos@gmail.com>
2020-08-16 06:24:52 +0900

    Test code for neural network.

---
+++ test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"PainTheMaster/mybraly/deeplearning"
+	"PainTheMaster/mybraly/deeplearning/mnist"
+	"fmt"
+	"os"
+)
+
+func main() {
+	actFuncHidden := []string{deeplearning.LabelIdentity, deeplearning.LabelReLU, deeplearning.LabelReLU, deeplearning.LabelReLU}
+	actFuncOut := deeplearning.LabelSoftMax
+
+	mnistDataLen := mnist.MnistCols * mnist.MnistRows
+	nodes := []int{mnistDataLen, 32, 32, 32, 10}
+	neuralNet := deeplearning.Make(nodes, actFuncHidden, actFuncOut)
+
+	neuralNet.ParamRMSProp.DecayRate = 0.9
+	neuralNet.ParamRMSProp.LearnRate = 0.001
+
+	neuralNet.ParamAdam.DecayRate1 = 0.9
+	neuralNet.ParamAdam.DecayRate2 = 0.999
+	neuralNet.ParamAdam.LearnRate = 0.001
+
+	neuralNet.ParamAdaDelta.DecayRate = 0.999
+
+	neuralNet.ParamMomentum.LearnRate = 0.005
+	neuralNet.ParamMomentum.MomentRate = 0.9
+
+	neuralNet.ParamGradDecent.LearnRate = 0.005
+
+	home := os.Getenv("HOME")
+	fmt.Println("Obtained home:", home)
+
+	trainImg, fileErr := os.Open(home + "/deeplearning/train-images-idx3-ubyte")
+	fmt.Println("test filepath", home+"/deeplearning/train-images-idx3-ubyte")
+	if fileErr != nil {
+		fmt.Println("Training image file open error:", fileErr)
+	}
+	trainLabel, fileErr := os.Open(home + "/deeplearning/train-labels-idx1-ubyte")
+	if fileErr != nil {
+		fmt.Println("Training label file open error:", fileErr)
+	}
+
+	miniBatchSize := 10
+	repetition := 15000
+	errHist := neuralNet.Train(trainImg, trainLabel, miniBatchSize, repetition, deeplearning.LabelAdam)
+
+	fmt.Println("Training finished:")
+	for i := range errHist {
+		fmt.Printf("%d,%f\n", i+1, errHist[i])
+	}
+
+	trainImg.Close()
+	trainLabel.Close()
+
+	testImg, fileErr := os.Open(home + "/deeplearning/t10k-images-idx3-ubyte")
+	if fileErr != nil {
+		fmt.Println("Test image file open error:", fileErr)
+	}
+	testLabel, fileErr := os.Open(home + "/deeplearning/t10k-labels-idx1-ubyte")
+	if fileErr != nil {
+		fmt.Println("Training label file open error:", fileErr)
+	}
+
+	testSize := 5000
+	accuracyPct := neuralNet.Test(testImg, testLabel, testSize)
+	fmt.Printf("Test accuracy with %d samples: %f%%", testSize, accuracyPct)
+}
